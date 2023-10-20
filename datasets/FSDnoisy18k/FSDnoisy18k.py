fe86f4d296aa253b266b6be168c0edf002accdc8
Shamasna <sulaiman.shamasna@iis.fraunhofer.de>
2023-10-20 18:38:48 +0200

    feat: :sparkles: Add new dataset generator

---
+++ datasets/FSDnoisy18k/FSDnoisy18k.py
@@ -0,0 +1,81 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import os
+import ffmpeg
+import glob
+from typing import Dict, Any, Generator
+
+def convert_to_mp3(file_path: str) -> bytes:
+    """
+    Converts a WAV audio file to MP3 format.
+
+    Args:
+        file_path (str): Path to the input WAV audio file.
+
+    Returns:
+        bytes: Binary MP3 audio data.
+    """
+    out, _ = (
+        ffmpeg.input(file_path)
+        .output(
+            "pipe:", ac=1, ar=16000, format="mp3", acodec="mp3", audio_bitrate="128k"
+        )
+        .run(capture_stdout=True, quiet=True)
+    )
+    return out
+
+class FSDNoisy18k(tfds.core.GeneratorBasedBuilder):
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
+        '1.0.0': 'Initial release.',
+    }
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """
+        Returns the dataset metadata.
+
+        Returns:
+            tfds.core.DatasetInfo: Metadata about the dataset.
+        """
+        return tfds.core.DatasetInfo(
+            builder=self,
+            features=tfds.features.FeaturesDict(
+                {
+                    "audio": tfds.features.Text(),
+                }
+            ),
+            homepage="https://zenodo.org/record/2529934#.Y0cLUYTP19M"
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager) -> Generator[tfds.core.SplitGenerator, None, None]:
+        """
+        Returns SplitGenerators.
+
+        Args:
+            dl_manager (tfds.download.DownloadManager): Download manager for dataset files.
+
+        Yields:
+            tfds.core.SplitGenerator: Split generators.
+        """
+        extracted_dir = dl_manager.download_and_extract("https://zenodo.org/record/2529934/files/FSDnoisy18k.audio_test.zip?download=1")
+
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                gen_kwargs={"directory": extracted_dir},
+            ),
+        ]
+
+    def _generate_examples(self, directory: str) -> Generator[Dict[str, Any], None, None]:
+        """
+        Yields examples.
+
+        Args:
+            directory (str): Path to the directory containing audio files.
+
+        Yields:
+            Dict[str, Any]: A dictionary with audio data.
+        """
+        for filename in glob.glob(os.path.join(directory, "FSDnoisy18k.audio_test", '*.wav')):
+            mp3_audio = convert_to_mp3(filename)
+            yield filename, {"audio": mp3_audio}
